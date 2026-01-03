a4aa80da42abc46813ef987197972fd654657a44
KarimZoPr0 <hamsterhaunter@gmail.com>
2026-01-03 04:44:31 +0000

    Code edited in Lovable Code Editor
    Edited UI in Lovable

--- src/components/playground/ExcalidrawPanel.tsx
+++ src/components/playground/ExcalidrawPanel.tsx
@@ -1,28 +1,11 @@
-import React from 'react';
-import { Excalidraw } from '@excalidraw/excalidraw';
-import '@excalidraw/excalidraw/index.css';
+import React from "react";
+import { Excalidraw } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/index.css";
 
 const ExcalidrawPanel: React.FC = () => {
   return (
-    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-      <Excalidraw
-        theme="dark"
-        initialData={{
-          appState: {
-            viewBackgroundColor: '#1a1a24',
-          },
-        }}
-        UIOptions={{
-          canvasActions: {
-            changeViewBackgroundColor: false,
-            export: false,
-            loadScene: false,
-            saveToActiveFile: false,
-            toggleTheme: false,
-            saveAsImage: false,
-          },
-        }}
-      />
+    <div className="h-screen w-full">
+      <Excalidraw theme="dark" />
     </div>
   );
 };
