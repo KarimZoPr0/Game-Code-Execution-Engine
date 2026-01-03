43f4b7faee113f627593635edee3a64590c6faa9
KarimZoPr0 <hamsterhaunter@gmail.com>
2026-01-03 04:50:13 +0000

    Reverted to commit 73ab2b0f7dfa991d6062c35de325959f48e29791

--- src/components/playground/ExcalidrawPanel.tsx
+++ src/components/playground/ExcalidrawPanel.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { Excalidraw } from "@excalidraw/excalidraw";
-import "@excalidraw/excalidraw/index.css";
+import React from 'react';
+import { Excalidraw } from '@excalidraw/excalidraw';
+import '@excalidraw/excalidraw/index.css';
 
 const ExcalidrawPanel: React.FC = () => {
   return (
-    <div className="h-screen w-full">
-      <Excalidraw theme="dark" />
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <Excalidraw
+        theme="dark"
+        initialData={{
+          appState: {
+            viewBackgroundColor: '#1a1a24',
+          },
+        }}
+        UIOptions={{
+          canvasActions: {
+            changeViewBackgroundColor: false,
+            export: false,
+            loadScene: false,
+            saveToActiveFile: false,
+            toggleTheme: false,
+            saveAsImage: false,
+          },
+        }}
+      />
     </div>
   );
 };
